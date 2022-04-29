0c50a4fc892656460165b796ae8efdac41b538f0
Tatjana Kunic <tanja.kunic@gmail.com>
2022-04-29 12:40:36 +0200

    Allow CORS in customer REST controller

--- src/main/java/com/tanja/web_customer_tracker/rest/CustomerRestController.java
+++ src/main/java/com/tanja/web_customer_tracker/rest/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.tanja.web_customer_tracker.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.tanja.web_customer_tracker.model.Customer;
 import com.tanja.web_customer_tracker.service.CustomerService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class CustomerRestController {
 
